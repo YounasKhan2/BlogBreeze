51f5a262af9afca1057b7e95a3cdadc8171cdc8f
younas <younaskk120@gmail.com>
2025-11-09 06:52:28 -0800

    adding superuser

---
+++ blog/management/commands/create_superuser.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = 'Create a superuser from environment variables'
+
+    def handle(self, *args, **options):
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'younas')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'younaskk120@gmail.com')
+        password = os.environ.get('2936667younasKK*')
+        
+        if not password:
+            self.stdout.write(self.style.ERROR('DJANGO_SUPERUSER_PASSWORD environment variable is required'))
+            return
+        
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" created successfully'))
+        else:
+            self.stdout.write(self.style.WARNING(f'Superuser "{username}" already exists'))
